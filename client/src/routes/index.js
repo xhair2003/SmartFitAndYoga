1a3c07526810cb752d5e3f43c0115a6e1c16c7b0
Bachtran301 <bachtranduy2003@gmail.com>
2024-10-18 21:53:54 +0700

    LandingPage

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,8 +1,14 @@
+import LandingPage from "../pages/LandingPage/LandingPage.jsx";
 import LoginPage from "../pages/LoginPage/LoginPage.jsx";
 
 export const routes = [
     {   
         id: 1,
+        path: '/',
+        page: LandingPage,
+    },
+    {   
+        id: 2,
         path: '/login',
         page: LoginPage,
     },
