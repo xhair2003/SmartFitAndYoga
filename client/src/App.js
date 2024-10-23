317ae036858c5790fd83a2a4dca0f733ed74aae3
Bachtran301 <bachtranduy2003@gmail.com>
2024-10-23 17:44:32 +0700

    NutritionPlansPage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 
+
 const App = () => {
   return (
     <div>
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,8 @@
 import LandingPage from "../pages/LandingPage/LandingPage.jsx";
 import LoginPage from "../pages/LoginPage/LoginPage.jsx";
+import HomePage from "../pages/HomePage/HomePage.jsx";
+import NutritionPlansPage from "../pages/NutritionPlansPage/NutritionPlan.jsx"
+
 
 export const routes = [
     {   
@@ -11,6 +14,16 @@ export const routes = [
         id: 2,
         path: '/login',
         page: LoginPage,
+    },
+    {   
+        id: 3,
+        path: '/home',
+        page: HomePage,
+    },
+    {   
+        id: 4,
+        path: '/nutrition',
+        page: NutritionPlansPage,
     },
 
     
