f7eb035da012b7d2e4a34e8e33d67cbfb096b426
Bachtran301 <bachtranduy2003@gmail.com>
2024-12-28 13:57:45 +0700

    [model] update

--- model/workout.py
+++ model/workout.py
@@ -112,4 +112,4 @@ def generate_weekly_workout_plan():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=6000)
+    app.run(host='0.0.0.0', port=6000, debug=True)
